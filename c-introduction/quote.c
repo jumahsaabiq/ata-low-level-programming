735f1ca00247ef1977fab85b2c3222254e14841c
jumahsaabiq <jumahmobolaji@gmail.com>
2025-09-19 12:32:25 +0100

    Add 3-line quote program

---
+++ c-introduction/quote.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main () {
+    printf("the only limit to achieving our tomorrow\n");
+    printf("is our doubts today\n");
+    printf("let us move forward with strong faith\n");
+    return 0;
+}
