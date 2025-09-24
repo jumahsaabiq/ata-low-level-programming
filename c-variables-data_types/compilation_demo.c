3464b3a2cf7a789fb1cf96c74544b8ac3a7a82f2
jumahsaabiq <jumahmobolaji@gmail.com>
2025-09-24 23:31:37 +0100

    Add program that prints Compilation in C works!

---
+++ c-variables-data_types/compilation_demo.c
@@ -0,0 +1,14 @@
+// This program demonstrates the compilation process by printing a message.
+
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * description: The entry point of our C program
+ * return: 0
+*/
+int main(void)
+{
+    printf("Compilation in C works!\n");
+    return 0;
+}
