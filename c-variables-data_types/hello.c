81c672ea7a451e250a891d7be03afe3487a9215a
jumahsaabiq <jumahmobolaji@gmail.com>
2025-09-24 23:09:00 +0100

    Add hello.c file

---
+++ c-variables-data_types/hello.c
@@ -0,0 +1,21 @@
+// This program prints a greeting message in C
+
+/*
+ * C is still important today because:
+ * 1. It is the foundation for many modern programming languages.
+ * 2. It is widely used in systems programming, operating systems, and embedded systems.
+ * 3. It provides efficiency, control, and portability for low-level tasks.
+ */
+
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * description: The entry point of our C program
+ * return: 0
+*/
+int main(void)
+{
+    printf("Hello, C programming!\n");
+    return 0;
+}
