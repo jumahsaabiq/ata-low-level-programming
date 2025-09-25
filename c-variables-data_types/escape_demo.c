f44d82b24e8f62ac5f4d41ff6271371f821c3e22
jumahsaabiq <jumahmobolaji@gmail.com>
2025-09-25 22:45:22 +0100

    Add escape_demo.c program

---
+++ c-variables-data_types/escape_demo.c
@@ -0,0 +1,17 @@
+// This program demonstrates the use of escape sequences \n and \t in C.
+
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * description: The entry point of our C program
+ * return: 0
+*/
+int main(void)
+{
+    printf("Line1\n");
+    printf("\tLine2\n");
+    printf("Tabbed\tText\n");
+
+    return 0;
+}
