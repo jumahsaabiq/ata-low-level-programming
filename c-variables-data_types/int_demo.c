697e1917937a524beb451eabc551fb64b1c6178d
jumahsaabiq <jumahmobolaji@gmail.com>
2025-09-25 22:36:39 +0100

    Add int_demo program

---
+++ c-variables-data_types/int_demo.c
@@ -0,0 +1,19 @@
+// This program demonstrates the use of int variables in C.
+
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * description: The entry point of our C program
+ * return: 0
+*/
+int main(void)
+{
+    int min = -100;
+    int max = 100;
+
+    printf("Min: %d\n", min);
+    printf("Max: %d\n", max);
+
+    return 0;
+}
