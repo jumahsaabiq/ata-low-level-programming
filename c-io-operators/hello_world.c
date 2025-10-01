e027457044f3e41e7305a8025ab461985b7a13a9
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-01 21:12:11 +0100

    Add program that prints hello world

---
+++ c-io-operators/hello_world.c
@@ -0,0 +1,12 @@
+/**
+ * main - the main function
+ * description: Prints "Hello, World!" to the screen
+ * return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello, World!\n");
+    return 0;
+}
