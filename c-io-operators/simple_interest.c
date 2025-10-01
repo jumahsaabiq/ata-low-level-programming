37f15b66866844e6cf573d39589524181a068792
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-01 22:17:09 +0100

    Add si program

---
+++ c-io-operators/simple_interest.c
@@ -0,0 +1,26 @@
+/**
+ * main - the main function
+ * description: Calculates Simple Interest from principal, rate, and time
+ * return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    float principal, rate, time, si;
+
+    printf("Enter principal: ");
+    scanf("%f", &principal);
+
+    printf("Enter rate of interest: ");
+    scanf("%f", &rate);
+
+    printf("Enter time (in years): ");
+    scanf("%f", &time);
+
+    si = (principal * rate * time) / 100;
+
+    printf("Simple Interest = %.2f\n", si);
+
+    return 0;
+}
