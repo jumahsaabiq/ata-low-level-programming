24a569ce50b5badf7308b1a09338d4ec5a1a74d1
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-03 00:47:13 +0100

    Add motivation2 prog

---
+++ c-io-operators/motivation_2.c
@@ -0,0 +1,14 @@
+/**
+ * main - the main function
+ * description: Prints motivational text using fputs with stdout
+ * return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    fputs("Keep pushing forward!\n", stdout);
+    fputs("Never give up.\n", stdout);
+
+    return 0;
+}
