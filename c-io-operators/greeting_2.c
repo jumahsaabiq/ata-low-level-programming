b5761eb77f77a8ea41d610c95cc0416f32401b04
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-03 00:40:05 +0100

    Add greeting2 prog

---
+++ c-io-operators/greeting_2.c
@@ -0,0 +1,14 @@
+/**
+ * main - the main function
+ * description: Prints greetings using puts function
+ * return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello, World!");
+    puts("Welcome to C programming.");
+
+    return 0;
+}
