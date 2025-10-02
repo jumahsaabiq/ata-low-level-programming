e833301b0c16f643bf8b6f82d05fd61eaec098d4
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-03 00:44:01 +0100

    Add banner2 prog

---
+++ c-io-operators/banner_2.c
@@ -0,0 +1,15 @@
+/**
+ * main - the main function
+ * description: Displays an ASCII art banner using puts function
+ * return: 0
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    puts("************");
+    puts("* C is Fun *");
+    puts("************");
+
+    return 0;
+}
