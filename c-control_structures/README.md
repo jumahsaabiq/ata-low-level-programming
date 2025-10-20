b7dac3b9981e30efc6a41fd7e8b6839a28e58d32
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-20 22:30:20 +0100

    Add pm_bin program

---
+++ c-control_structures/README.md
@@ -0,0 +1,4 @@
+# Control Structures in C
+
+This project explores one of the core concepts in C programming — control structures.
+Control structures allow you to decide the flow of execution in your program, make decisions, and repeat tasks efficiently.
---
+++ c-control_structures/positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - determines if an integer is positive, negative, or zero
+ * description: The program takes an integer input and prints whether
+ * it is positive, negative, or zero using if...else statements.
+ * return: 0
+*/
+
+int main(void)
+{
+    int number;
+
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number > 0)
+    {
+        printf("%d is a positive number.\n", number);
+    }
+    else if (number < 0)
+    {
+        printf("%d is a negative number.\n", number);
+    }
+    else
+    {
+        printf("%d is zero.\n", number);
+    }
+
+    return 0;
+}
