bdd192d09fd722a162a7523969688a0f22874803
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-26 23:50:22 +0100

    Fix: to lyc_bin program

--- c-control_structures/leap_year_checker.c
+++ c-control_structures/leap_year_checker.c
@@ -4,9 +4,15 @@ int main() {
     int year;
 
     printf("Enter a year: ");
-    scanf("%d", &year);
+    if (scanf("%d", &year) != 1) {
+        return 1; // invalid input
+    }
 
-    // Leap year condition
+    if (year <= 0) {
+        printf("%d is not a leap year.\n", year);
+        return 0;
+    }
+
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
         printf("%d is a leap year.\n", year);
     } else {
