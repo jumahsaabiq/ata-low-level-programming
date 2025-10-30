a53282595dfbd098f0e51436f7eb802d707d39b2
jumahsaabiq <jumahmobolaji@gmail.com>
2025-10-30 22:56:57 +0100

    Add 0-hello_bin program

---
+++ c-functions/0-simple_greeting.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_hello - prints a greeting message
+ * Description: This function prints "Hello, C Functions!"
+ */
+void print_hello(void)
+{
+    printf("Hello, C Functions!\n");
+}
+
+/**
+ * main - entry point of the program
+ * Return: 0 on success
+ */
+int main(void)
+{
+    print_hello();
+    return 0;
+}
