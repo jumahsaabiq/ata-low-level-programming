c2b8d7c2a6309ba5dad2ffba3df9280c37304738
jumahsaabiq <jumahmobolaji@gmail.com>
2025-11-01 20:21:21 +0100

    Fix: to 0-factorial.c

--- c-recursion/0-factorial.c
+++ c-recursion/0-factorial.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-
 /**
  * factorial - recursively calculates the factorial of a non-negative integer
  * @n: the integer whose factorial is to be calculated
@@ -8,28 +6,8 @@
  */
 long long factorial(int n)
 {
-    if (n == 0 || n == 1)  /* Base case */
+    if (n == 0 || n == 1)
         return 1;
-    else                   /* Recursive case */
+    else
         return n * factorial(n - 1);
-}
-
-int main(void)
-{
-    int n;
-    long long result;
-
-    printf("Enter an integer: ");
-    scanf("%d", &n);
-
-    if (n < 0)
-    {
-        printf("Factorial is not defined for negative numbers.\n");
-        return 1;
-    }
-
-    result = factorial(n);
-    printf("The factorial of %d is: %lld\n", n, result);
-
-    return 0;
 }
