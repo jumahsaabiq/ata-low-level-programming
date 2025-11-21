78449375938d0365de20f36bfad42fecd0307894
jumahsaabiq <jumahmobolaji@gmail.com>
2025-11-21 10:09:56 +0100

    Add sq_bin program

---
+++ c-variadic-functions/2-square.c
@@ -0,0 +1,10 @@
+/**
+ * square - calculates the square of an integer
+ * @num: the integer to square
+ *
+ * Return: the square of num
+ */
+static inline int square(int num)
+{
+    return num * num;
+}
