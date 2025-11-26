3eea24cf7fe90323b5808c84a48ade12249e18fe
jumahsaabiq <jumahmobolaji@gmail.com>
2025-11-26 17:41:01 +0100

    Fix: to io_program

--- c-standard-libraries/0-basic_io.c
+++ c-standard-libraries/0-basic_io.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - demonstrates basic input/output using stdio.h
@@ -8,20 +9,37 @@
 int main(void)
 {
     char name[100];
+    char age_str[20];
+    char height_str[20];
     int age;
     float height;
 
     /* Prompt for name */
     printf("Enter your name: ");
-    scanf("%99s", name);
+    fflush(stdout);
+    fgets(name, sizeof(name), stdin);
+
+    /* Remove newline */
+    for (int i = 0; name[i]; i++)
+    {
+        if (name[i] == '\n')
+        {
+            name[i] = '\0';
+            break;
+        }
+    }
 
     /* Prompt for age */
     printf("Enter your age: ");
-    scanf("%d", &age);
+    fflush(stdout);
+    fgets(age_str, sizeof(age_str), stdin);
+    age = atoi(age_str);
 
     /* Prompt for height */
     printf("Enter your height (in meters): ");
-    scanf("%f", &height);
+    fflush(stdout);
+    fgets(height_str, sizeof(height_str), stdin);
+    height = atof(height_str);
 
     /* Output formatted info */
     printf("Hello, %s! You are %d years old and %.2f meters tall.\n",
