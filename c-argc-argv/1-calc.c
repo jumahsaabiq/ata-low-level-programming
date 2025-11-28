f732ab28f48f632e4cf5bb7c9c75fce0ef632c78
jumahsaabiq <jumahmobolaji@gmail.com>
2025-11-28 16:56:42 +0100

    Fix: to calc program

--- c-argc-argv/1-calc.c
+++ c-argc-argv/1-calc.c
@@ -2,44 +2,40 @@
 #include <stdlib.h>
 
 /**
- * main - simple command-line calculator (addition and subtraction)
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: 0
- */
+ * main - simple command-line calculator
+ * description: performs addition or subtraction on two integers
+ * return: 0 on success, 1 on error
+*/
 int main(int argc, char *argv[])
 {
     int num1, num2, result;
     char op;
 
-    /* Check for exactly 3 arguments */
     if (argc != 4)
     {
         printf("Usage: ./calc <number1> <operator> <number2>\n");
-        return 0;
+        return 1;  /* error exit */
     }
 
     num1 = atoi(argv[1]);
     op = argv[2][0];
     num2 = atoi(argv[3]);
 
-    /* Operator handling */
     if (op == '+')
     {
         result = num1 + num2;
+        printf("Result: %d\n", result);
     }
     else if (op == '-')
     {
         result = num1 - num2;
+        printf("Result: %d\n", result);
     }
     else
     {
         printf("Error: Invalid operator. Use '+' or '-'.\n");
-        return 0;
+        return 1; /* error exit */
     }
-
-    printf("Result: %d\n", result);
 
     return 0;
 }
